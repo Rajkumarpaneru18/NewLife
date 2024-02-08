a8745ac0378ae98baac82fd61e962d218934a9a0
Rajkumarpaneru18 <rajkumarpaneru111@gmail.com>
2024-02-08 18:31:12 +0545

    Defined overall layout structure with header, footer, hero, features, and testimonials components

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,15 @@
-import PropsTypes from "prop-types";
+import PropTypes from "prop-types";
 
 const Layout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="bg-gradient-to-br from-purple-700 via-orange-300 to-purple-700 min-h-screen">
+      {children}
+    </div>
+  );
 };
 
-Layout.propTypes  ={
-    children: PropsTypes.element
-}
+Layout.propTypes = {
+  children: PropTypes.element,
+};
 
 export default Layout;
