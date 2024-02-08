32dfd9672785d4fbace7fa7dd66b4fee85cfbe2d
Rajkumarpaneru18 <rajkumarpaneru111@gmail.com>
2024-02-08 17:02:28 +0545

    enhance the feature section with image and styling

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import Feature from "../../components/LandingPage/Feature";
+import Feature from "../../components/Feature";
 import Footer from "../../components/LandingPage/Footer";
 import Header from "../../components/Header/";
 import Testimonial from "../../components/LandingPage/Testimonial";
