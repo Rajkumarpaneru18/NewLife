327346774587371f3c820492c461c43521c4db8d
Rajkumarpaneru18 <rajkumarpaneru111@gmail.com>
2024-02-09 14:51:46 +0545

    setup routes for chat

---
+++ src/pages/Chat/Chat.jsx
@@ -0,0 +1,13 @@
+import Layout from "../../layout/Layout";
+
+const Chat = () => {
+  return (
+    <>
+      <Layout>
+        <div className="w-screen h-screen text-white">This is chat section</div>
+      </Layout>
+    </>
+  );
+};
+
+export default Chat;
--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage/LandingPage";
+import Chat from "../pages/Chat";
 
 const AppRoute = () => {
   return (
     <Router>
         <Routes>
             <Route path="/" element={<LandingPage/>}/>
+            <Route path="/chat" element={<Chat/>}/>
         </Routes>
     </Router>
   )
