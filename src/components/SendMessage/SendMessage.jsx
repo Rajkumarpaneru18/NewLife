68190a8abab35dc4a48b493ce8639dee94ace89a
Rajkumarpaneru18 <rajkumarpaneru111@gmail.com>
2024-02-09 15:21:03 +0545

    create send message component

--- src/components/SendMessage/SendMessage.jsx
+++ src/components/SendMessage/SendMessage.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import { IoSend } from "react-icons/io5";
 
-const SendMessage = () => {
+const SendMessage = ({ sendMessage }) => {
+  const [message, setMessage] = useState("");
+
   return (
-    <div>SendMessage</div>
-  )
-}
+    <div className="flex justify-between items-center px-4 py-3  border-t border-gray-600 bg-black">
+      <form className="flex-grow mx-4">
+        <input
+          className="w-full p-3 text-base text-white placeholder-gray-500 bg-gray-700 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:border-transparent resize-none"
+          placeholder="Type your message here..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+      <button
+        onClick={() => {
+          console.log("Message send");
+          setMessage("");
+          sendMessage(message);
+        }}
+        className="p-3 bg-indigo-500 rounded-full text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition duration-150 ease-in-out"
+      >
+        <IoSend className="w-6 h-6" />
+      </button>
+    </div>
+  );
+};
 
-export default SendMessage
+export default SendMessage;
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
+
 import Messages from "../../components/Messages";
 import SendMessage from "../../components/SendMessage";
 import Layout from "../../layout/Layout";
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+
+  const addMessage = (text, sender) => {
+    setMessages((prevMessages) => [...prevMessages, { text, sender }]);
+  };
+
+  const sendMessage = (text) => {
+    console.log(text);
+    addMessage(text, "user");
+
+    try {
+      // API Call
+      console.log("Data added");
+    } catch (error) {
+      console.error("Error sending message:", error);
+      addMessage("Error occurred", "ai");
+    }
+  };
   return (
     <>
       <Layout>
-        <div className="w-screen h-screen text-white">
-          <Messages />
-          <SendMessage />
+        <div className="w-[80vw] max-w-[80vw] text-white">
+          <SendMessage sendMessage={sendMessage} />
         </div>
       </Layout>
     </>
